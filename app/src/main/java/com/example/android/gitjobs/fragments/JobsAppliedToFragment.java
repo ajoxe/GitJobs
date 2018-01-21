4f612019c533142ee4073918fa8a2a9d0e6a87f8
Amirah Oxendine <amirahoxendine@ac.c4q.nyc>
2018-01-21 17:08:10 -0500

    added fragments

--- app/src/main/java/com/example/android/gitjobs/fragments/JobsAppliedToFragment.java
+++ app/src/main/java/com/example/android/gitjobs/fragments/JobsAppliedToFragment.java
@@ -1,15 +1,24 @@
 package com.example.android.gitjobs.fragments;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.android.gitjobs.GitJobsDBHelper;
 import com.example.android.gitjobs.R;
+import com.example.android.gitjobs.controller.GitJobsAdapter;
 import com.example.android.gitjobs.model.GitJobsModel;
 
 import java.util.List;
@@ -21,6 +30,15 @@ public class JobsAppliedToFragment extends Fragment {
     View rootView;
     List<GitJobsModel> appliedJobsList;
     GitJobsDBHelper db;
+
+    RecyclerView appliedRecyclerView;
+    GitJobsAdapter appliedAdapter;
+    LinearLayoutManager linearLayoutManager;
+    Context context;
+
+    View.OnClickListener searchListButtonClick;
+    View.OnClickListener saveButtonClick;
+    View.OnClickListener applyButtonClick;
 
     public JobsAppliedToFragment() {
         // Required empty public constructor
@@ -35,7 +53,63 @@ public class JobsAppliedToFragment extends Fragment {
         db = new GitJobsDBHelper(getActivity());
         appliedJobsList = db.getAppliedJobsList();
         Log.d("applied fragment", "list size : " + appliedJobsList.size());
+        context = getContext().getApplicationContext();
+        setUpRecyclerView();
         return rootView;
+    }
+
+    public void setUpRecyclerView(){
+        setApplyButton();
+        setSaveButton();
+        setSearchListButton();
+        appliedRecyclerView = (RecyclerView) rootView.findViewById(R.id.applied_list_recyclerview);
+        appliedAdapter = new GitJobsAdapter(appliedJobsList, context, searchListButtonClick,saveButtonClick,applyButtonClick);
+        linearLayoutManager = new LinearLayoutManager(context);
+        appliedRecyclerView.setLayoutManager(linearLayoutManager);
+        appliedRecyclerView.setAdapter(appliedAdapter);
+    }
+
+    public void setApplyButton(){
+        applyButtonClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String jobId = v.getTag().toString();
+                GitJobsModel job = db.getJobById(jobId);
+                Intent applyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(job.getUrl()));
+                startActivity(applyIntent);
+                Toast.makeText(getActivity(), "Good Luck!", Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+    public void setSaveButton(){
+        saveButtonClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String jobId = v.getTag().toString();
+
+                Toast.makeText(getActivity(), "This job is saved to your 'saved' list!", Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+    public void setSearchListButton(){
+        searchListButtonClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String jobId = v.getTag().toString();
+                GitJobsDetailFragment detailFragment = new GitJobsDetailFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString("job_id", jobId);
+                detailFragment.updateId(bundle);
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit);
+                fragmentTransaction.replace(R.id.main_fragment_container, detailFragment);
+                fragmentTransaction.addToBackStack("next");
+                fragmentTransaction.commit();
+            }
+        };
     }
 
 }
--- app/src/main/java/com/example/android/gitjobs/fragments/SavedJobsFragment.java
+++ app/src/main/java/com/example/android/gitjobs/fragments/SavedJobsFragment.java
@@ -1,18 +1,31 @@
 package com.example.android.gitjobs.fragments;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.android.gitjobs.GitJobsDBHelper;
 import com.example.android.gitjobs.R;
+import com.example.android.gitjobs.controller.GitJobsAdapter;
 import com.example.android.gitjobs.model.GitJobsModel;
 
 import java.util.List;
+
+import static com.example.android.gitjobs.GitJobsDBContract.GitJobsentry._STATUS_APPLIED;
+import static com.example.android.gitjobs.GitJobsDBContract.GitJobsentry._STATUS_SAVED;
+import static com.example.android.gitjobs.GitJobsDBContract.GitJobsentry._STATUS_SEARCHED;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +34,13 @@ public class SavedJobsFragment extends Fragment {
     View rootView;
     List<GitJobsModel> savedJobsList;
     GitJobsDBHelper db;
+    RecyclerView savedRecyclerView;
+    GitJobsAdapter savedAdapter;
+    LinearLayoutManager linearLayoutManager;
+    Context context;
+    View.OnClickListener searchListButtonClick;
+    View.OnClickListener saveButtonClick;
+    View.OnClickListener applyButtonClick;
 
 
     public SavedJobsFragment() {
@@ -36,8 +56,65 @@ public class SavedJobsFragment extends Fragment {
         db = new GitJobsDBHelper(getActivity());
         savedJobsList = db.getSavedJobsList();
         Log.d("saved fragment", "list size : " + savedJobsList.size());
+        context = getContext().getApplicationContext();
+        setUpRecyclerView();
+
 
         return rootView;
+    }
+
+    public void setUpRecyclerView(){
+        setApplyButton();
+        setSaveButton();
+        setSearchListButton();
+        savedRecyclerView = (RecyclerView) rootView.findViewById(R.id.saved_list_recyclerview);
+        savedAdapter = new GitJobsAdapter(savedJobsList, context, searchListButtonClick,saveButtonClick,applyButtonClick);
+        linearLayoutManager = new LinearLayoutManager(context);
+        savedRecyclerView.setLayoutManager(linearLayoutManager);
+        savedRecyclerView.setAdapter(savedAdapter);
+    }
+
+    public void setApplyButton(){
+        applyButtonClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String jobId = v.getTag().toString();
+                GitJobsModel job = db.getJobById(jobId);
+                Intent applyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(job.getUrl()));
+                startActivity(applyIntent);
+                Toast.makeText(getActivity(), "Good Luck!", Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+    public void setSaveButton(){
+        saveButtonClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String jobId = v.getTag().toString();
+
+                Toast.makeText(getActivity(), "This job is saved to your 'saved' list!", Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+    public void setSearchListButton(){
+        searchListButtonClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String jobId = v.getTag().toString();
+                GitJobsDetailFragment detailFragment = new GitJobsDetailFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString("job_id", jobId);
+                detailFragment.updateId(bundle);
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit);
+                fragmentTransaction.replace(R.id.main_fragment_container, detailFragment);
+                fragmentTransaction.addToBackStack("next");
+                fragmentTransaction.commit();
+            }
+        };
     }
 
 }
